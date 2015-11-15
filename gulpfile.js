a83acc49da67052c77af3526f53c988ec6421d5f
Thada Wangthammang <mildronize@gmail.com>
2015-11-15 21:55:00 +0700

    Update 2015-11-15T14:54:50.958Z

--- gulpfile.js
+++ gulpfile.js
@@ -2,22 +2,22 @@ var gulp = require('gulp');
 var webserver = require('gulp-webserver');
 var ghPages = require('gulp-gh-pages');
 
-gulp.task('webserver', function() {
-  gulp.src('slides')
-    .pipe(webserver({
-      livereload: true,
-      open: true
-    }));
-});
+// gulp.task('webserver', function() {
+  // gulp.src('slides')
+    // .pipe(webserver({
+      // livereload: true,
+      // open: true
+    // }));
+// });
 
-gulp.task('watch', function() {
-  gulp.src('resources/*.css'),
-  gulp.src('resources/fonts/*')
-})
+// gulp.task('watch', function() {
+  // gulp.src('resources/*.css'),
+  // gulp.src('resources/fonts/*')
+// })
 
 gulp.task('deploy', function() {
-  return gulp.src('./slides/**/*')
+  return gulp.src('./**/*')
     .pipe(ghPages());
 });
 
-gulp.task('serve', ['webserver','watch']);
+// gulp.task('serve', ['webserver','watch']);
